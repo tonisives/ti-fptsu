e9bc6e2367d6c74ace1900116255dae664c2fcda
tonisives <tonisives@gmail.com>
2024-05-20 17:00:30 +0700

    update

--- dist/log.test.js
+++
@@ -1,8 +0,0 @@
-import { pipe } from "fp-ts/lib/function.js";
-import * as TE from "fp-ts/lib/TaskEither.js";
-import { logC } from "./log.js";
-import { it } from "vitest";
-it("should log", async () => {
-    await pipe(TE.of(0), logC("green")((it) => `The number is ${it}`))();
-});
-//# sourceMappingURL=log.test.js.map
