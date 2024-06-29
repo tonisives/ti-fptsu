a07a5be7594298fcba38a668c45597e413aad074
tonisives <tonisives@gmail.com>
2024-06-29 12:18:38 +0700

    update

---
+++ dist/lib.d.ts
@@ -0,0 +1,7 @@
+export * as O from "fp-ts/lib/Option.js";
+export * as TE from "fp-ts/lib/TaskEither.js";
+export * as E from "fp-ts/lib/Either.js";
+export * as T from "fp-ts/lib/Task.js";
+export * as A from "fp-ts/lib/Array.js";
+import * as fun from "fp-ts/lib/function.js";
+export declare let pipe: typeof fun.pipe;
---
+++ dist/lib.js
@@ -0,0 +1,9 @@
+// one possible problem when adding this file: Are all of these exports included in the final bundle?
+export * as O from "fp-ts/lib/Option.js";
+export * as TE from "fp-ts/lib/TaskEither.js";
+export * as E from "fp-ts/lib/Either.js";
+export * as T from "fp-ts/lib/Task.js";
+export * as A from "fp-ts/lib/Array.js";
+import * as fun from "fp-ts/lib/function.js";
+export let pipe = fun.pipe;
+//# sourceMappingURL=lib.js.map
