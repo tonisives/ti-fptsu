47d424625f0c11d6a8085ec5507bf72fba2eb86f
tonisives <tonisives@gmail.com>
2024-08-31 07:36:37 +0300

    update

--- dist/validate.d.ts
+++ dist/validate.d.ts
@@ -1,13 +1,17 @@
 import { E } from "./lib.js";
 /**
- pipe(
-  E.Do,
-  apS("jwt", validateJwt(event)),
-  apS("user", validateUser(event)),
-  E.fold(
-    (err) => E.left(new Error(err)),
-    (validated) => E.right(validated)
-  )
- )
+ * concat all errors into a single string (apS)
+ *
+ * @example
+ * let validateInput = (body: any): E.Either<Error, Input> =>
+ *  pipe(
+ *    E.Do,
+ *    apSc("traces", body.traces ? E.right(body.traces) : E.left("traces is required")),
+ *    apSc("config", body.config ? E.right(body.config) : E.left("config is required")),
+ *    E.fold(
+ *      (err) => E.left(new Error(err)),
+ *      (validated) => E.right(validated as Input),
+ *    ),
+ *  )
  */
-export declare let commaValidate: <N extends string, A, B>(name: Exclude<N, keyof A>, fb: E.Either<string, B>) => (fa: E.Either<string, A>) => E.Either<string, { readonly [K in N | keyof A]: K extends keyof A ? A[K] : B; }>;
+export declare let apS: <N extends string, A, B>(name: Exclude<N, keyof A>, fb: E.Either<string, B>) => (fa: E.Either<string, A>) => E.Either<string, { readonly [K in N | keyof A]: K extends keyof A ? A[K] : B; }>;
--- dist/validate.js
+++ dist/validate.js
@@ -3,15 +3,19 @@ import { E, pipe } from "./lib.js";
 import * as S from "fp-ts/lib/Semigroup.js";
 import * as A from "fp-ts/lib/Apply.js";
 /**
- pipe(
-  E.Do,
-  apS("jwt", validateJwt(event)),
-  apS("user", validateUser(event)),
-  E.fold(
-    (err) => E.left(new Error(err)),
-    (validated) => E.right(validated)
-  )
- )
+ * concat all errors into a single string (apS)
+ *
+ * @example
+ * let validateInput = (body: any): E.Either<Error, Input> =>
+ *  pipe(
+ *    E.Do,
+ *    apSc("traces", body.traces ? E.right(body.traces) : E.left("traces is required")),
+ *    apSc("config", body.config ? E.right(body.config) : E.left("config is required")),
+ *    E.fold(
+ *      (err) => E.left(new Error(err)),
+ *      (validated) => E.right(validated as Input),
+ *    ),
+ *  )
  */
-export let commaValidate = A.apS(E.getApplicativeValidation(pipe(string.Semigroup, S.intercalate(", "))));
+export let apS = A.apS(E.getApplicativeValidation(pipe(string.Semigroup, S.intercalate(", "))));
 //# sourceMappingURL=validate.js.map
